19d285c0ad6b077331cb083b3665390b8dc74d30
Michał Idzik <idzik@student.agh.edu.pl>
2015-01-24 20:15:20 +0100

    Extended pareto fronts plottter

--- best_fronts_plotter.py
+++ best_fronts_plotter.py
@@ -1,8 +1,12 @@
 import os
 import pickle
+
 import matplotlib.pyplot as plt
 from pathlib import Path
-import sys
+
+from ep.utils import ea_utils
+from pictures_from_stats_new import algos
+
 
 PLOTS_DIR = 'plots'
 RESULTS_DIR = 'pareto_results'
@@ -10,7 +14,7 @@ RESULTS_DIR = 'pareto_results'
 metrics_name_long = "distance_from_pareto"
 
 
-def plot_results(best_result, original_front):
+def plot_problem_front(original_front, multimodal=False, scatter=False):
     f = plt.figure(num=None, facecolor='w', edgecolor='k')
 
     plt.xlabel('1st objective', fontsize=20)
@@ -19,18 +23,37 @@ def plot_results(best_result, original_front):
     plt.axhline(linestyle='--', lw=0.9, c='#7F7F7F')
     plt.axvline(linestyle='--', lw=0.9, c='#7F7F7F')
 
-    prto_x = [x[0] for x in original_front]
-    prto_y = [x[1] for x in original_front]
-    plt.scatter(prto_x, prto_y, c='0.8', s=80, alpha=0.3)
+    plt.margins(y=.1, x=.1)
+
+    if multimodal:
+        subfronts = ea_utils.split_front(original_front, 0.05)
+        for front in subfronts:
+            plot_front(front, scatter)
+    else:
+        plot_front(original_front, scatter)
+
+    return f
+
+
+def plot_front(series, scatter=False):
+    x = [x[0] for x in series]
+    y = [x[1] for x in series]
+    plt.plot(x, y, c='k', lw=6, alpha=0.6)
+
+
+def plot_results(f, best_result):
+    name, _, markers, color = algos[best_result['algorithm']]
 
     res_x = [x[0] for x in best_result['result']]
     res_y = [x[1] for x in best_result['result']]
-    plt.scatter(res_x, res_y, c='k')
+    plt.scatter(res_x, res_y, marker=markers, color=color)
 
+
+def save_plot(f):
     os.makedirs(PLOTS_DIR, exist_ok=True)
     os.makedirs(os.path.join(PLOTS_DIR, RESULTS_DIR), exist_ok=True)
 
-    fig_path = os.path.join(PLOTS_DIR,  RESULTS_DIR, '{}.eps'.format(d_problem.name.replace('emoa', 'moea')))
+    fig_path = os.path.join(PLOTS_DIR, RESULTS_DIR, '{}.eps'.format(d_problem.name.replace('emoa', 'moea')))
     plt.savefig(fig_path)
     plt.close(f)
 
@@ -42,10 +65,18 @@ if __name__ == '__main__':
     for d_problem in [p_problem
                       for p_problem in root.iterdir()
                       if p_problem.is_dir()]:
-        best_result = None
+
+
+        problem_mod = __import__('problems.{}.problem'.format(d_problem.name), fromlist=[d_problem.name])
+        original_front = problem_mod.pareto_front
+        f = plot_problem_front(original_front, multimodal=d_problem.name == 'coemoa_c', scatter=d_problem.name == 'ackley')
+
         for d_algorithm in [p_algo
                             for p_algo in d_problem.iterdir()
                             if p_algo.is_dir()]:
+
+            best_result = None
+
             for d_testname in [p_testname
                                for p_testname in d_algorithm.iterdir()
                                if p_testname.is_dir()]:
@@ -56,14 +87,13 @@ if __name__ == '__main__':
                                 if p_budget.is_dir()],
                                key=lambda x: int(x.name))
 
-
                 for results in d_budget.glob(metrics_name_long + "*.pickle"):
-                        with results.open(mode="rb") as fh:
-                            test_results = pickle.load(fh)
-                            if not best_result or  test_results["metrics"] < best_result["metrics"]:
-                                best_result = test_results
+                    with results.open(mode="rb") as fh:
+                        test_results = pickle.load(fh)
+                        if not best_result or test_results["metrics"] < best_result["metrics"]:
+                            best_result = test_results
 
-        problem_mod = __import__('problems.{}.problem'.format(d_problem.name), fromlist=[d_problem.name])
-        original_front = problem_mod.pareto_front
-        plot_results(best_result, original_front)
+            plot_results(f, best_result)
+
+        save_plot(f)
 
--- pictures_from_stats_new.py
+++ pictures_from_stats_new.py
@@ -20,16 +20,19 @@ import matplotlib.pyplot as plt
 SPEA_LS = '-'
 NSGA_LS = '--'
 IBEA_LS = ':'
+SPEA_M = 'o'
+NSGA_M = '*'
+IBEA_M = '^'
 BARE_CL = '0.8'
 IMGA_CL = '0.4'
 HGS_CL = '0.0'
 
-algos = {'spea2': ('SPEA2', SPEA_LS, BARE_CL), 'nsga2': ('NSGAII', NSGA_LS, BARE_CL),
-         'ibea': ( 'IBEA', IBEA_LS, BARE_CL),
-         'imga_spea2': ('IMGA-SPEA2', SPEA_LS, IMGA_CL),
-         'imga_nsga2': ( 'IMGA-NSGAII', NSGA_LS, IMGA_CL), 'imga_ibea': ('IMGA-IBEA', IBEA_LS, IMGA_CL),
-         'hgs_spea2': ( 'HGS-SPEA2', SPEA_LS, HGS_CL),
-         'hgs_nsga2': ( 'HGS-NSGAII', NSGA_LS, HGS_CL), 'hgs_ibea': ( 'HGS-IBEA', IBEA_LS, HGS_CL)}
+algos = {'spea2': ('SPEA2', SPEA_LS, SPEA_M, BARE_CL), 'nsga2': ('NSGAII', NSGA_LS, NSGA_M, BARE_CL),
+         'ibea': ( 'IBEA', IBEA_LS, IBEA_M, BARE_CL),
+         'imga_spea2': ('IMGA-SPEA2', SPEA_LS, SPEA_M,  IMGA_CL),
+         'imga_nsga2': ( 'IMGA-NSGAII', NSGA_LS,NSGA_M, IMGA_CL), 'imga_ibea': ('IMGA-IBEA', IBEA_LS, IBEA_M, IMGA_CL),
+         'hgs_spea2': ( 'HGS-SPEA2', SPEA_LS, SPEA_M, HGS_CL),
+         'hgs_nsga2': ( 'HGS-NSGAII', NSGA_LS, NSGA_M, HGS_CL), 'hgs_ibea': ( 'HGS-IBEA', IBEA_LS, IBEA_M, HGS_CL)}
 
 algos_order = ['spea2', 'nsga2', 'ibea', 'imga_spea2', 'imga_nsga2', 'imga_ibea', 'hgs_spea2', 'hgs_nsga2', 'hgs_ibea']
 
@@ -242,7 +245,7 @@ def plot_legend(series):
     figlegend.legend(series, [s.get_label() for s in series], 'center', prop={'size': 10},
                      handlelength=8, borderpad=1.2, labelspacing=1, frameon=False)
 
-    figlegend.savefig(os.path.join(PLOTS_DIR, 'plots_bnw', 'legend.pdf'))
+    figlegend.savefig(os.path.join(PLOTS_DIR, 'plots_bnw', 'legend.eps'))
 
 
 def plot_results(results):
@@ -287,14 +290,14 @@ def plot_results(results):
         plot_data = dict(plot_data)
         for algo in algos_order:
             data = plot_data[algo]
-            name, lines, color = algos[algo]
+            name, lines, marker, color = algos[algo]
             (xs, xerr), (ys, yerr) = data
             if 'nsga2' in algo:
                 ms = base_ms + 1
             else:
                 ms = base_ms
 
-            last_plt.append(plt.plot(xs, ys, ls=lines, color=color, label=name, linewidth=lw, ms=ms)[0])
+            last_plt.append(plt.plot(xs, ys, ls=lines, color=color, label=name, marker=marker, linewidth=lw, ms=ms)[0])
 
         problem, metric = plot_name
 
