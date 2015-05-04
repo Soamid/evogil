b570a685f70384cb50267b6f06822a5ee20a58b1
Konrad Gądek <kgadek@gmail.com>
2015-05-04 11:48:29 +0200

    Run order

--- evotools/config.py
+++ evotools/config.py
@@ -23,71 +23,71 @@ metric_names = [ ("distance_from_pareto", "dst from pareto"),
                  ("extent", "extent")
                ]
 
-run_rder = [ ('coemoa_e', 'hgs_nsga2' ),
-             ('coemoa_d', 'hgs_nsga2' ),
-             ('coemoa_d', 'imga_nsga2'),
-             ('coemoa_c', 'hgs_nsga2' ),
-             ('coemoa_a', 'hgs_nsga2' ),
-             ('coemoa_b', 'hgs_nsga2' ),
-             ('coemoa_d', 'nsga2'     ),
-             ('coemoa_e', 'nsga2'     ),
-             ('kursawe',  'hgs_nsga2' ),
-             ('coemoa_c', 'nsga2'     ),
-             ('coemoa_a', 'nsga2'     ),
-             ('coemoa_b', 'nsga2'     ),
-             ('coemoa_e', 'imga_nsga2'),
-             ('coemoa_c', 'imga_nsga2'),
-             ('coemoa_b', 'imga_nsga2'),
-             ('coemoa_a', 'imga_nsga2'),
-             ('coemoa_b', 'hgs_spea2' ),
-             ('coemoa_a', 'hgs_spea2' ),
-             ('kursawe',  'imga_nsga2'),
-             ('kursawe',  'nsga2'     ),
-             ('coemoa_d', 'hgs_spea2' ),
-             ('coemoa_d', 'ibea'      ),
-             ('coemoa_d', 'imga_ibea' ),
-             ('kursawe',  'ibea'      ),
-             ('kursawe',  'imga_ibea' ),
-             ('kursawe',  'spea2'     ),
-             ('kursawe',  'imga_spea2'),
-             ('coemoa_e', 'ibea'      ),
-             ('coemoa_e', 'imga_ibea' ),
-             ('kursawe',  'hgs_spea2' ),
-             ('coemoa_c', 'hgs_spea2' ),
-             ('coemoa_c', 'ibea'      ),
-             ('coemoa_c', 'imga_ibea' ),
-             ('coemoa_d', 'imga_spea2'),
-             ('coemoa_b', 'imga_ibea' ),
-             ('coemoa_b', 'ibea'      ),
-             ('coemoa_a', 'ibea'      ),
-             ('kursawe',  'hgs_ibea'  ),
-             ('coemoa_a', 'imga_ibea' ),
-             ('coemoa_d', 'hgs_ibea'  ),
-             ('coemoa_e', 'hgs_spea2' ),
-             ('coemoa_c', 'hgs_ibea'  ),
-             ('coemoa_a', 'hgs_ibea'  ),
-             ('coemoa_b', 'hgs_ibea'  ),
-             ('coemoa_a', 'imga_spea2'),
-             ('coemoa_b', 'imga_spea2'),
-             ('coemoa_c', 'imga_spea2'),
-             ('coemoa_e', 'imga_spea2'),
-             ('ackley',   'imga_nsga2'),
-             ('ackley',   'hgs_nsga2' ),
-             ('coemoa_d', 'spea2'     ),
-             ('coemoa_a', 'spea2'     ),
-             ('coemoa_e', 'spea2'     ),
-             ('coemoa_b', 'spea2'     ),
-             ('coemoa_c', 'spea2'     ),
-             ('ackley',   'nsga2'     ),
-             ('coemoa_e', 'hgs_ibea'  ),
-             ('ackley',   'imga_spea2'),
-             ('ackley',   'hgs_spea2' ),
-             ('ackley',   'spea2'     ),
-             ('ackley',   'imga_ibea' ),
-             ('ackley',   'ibea'      ),
-             ('ackley',   'hgs_ibea'  ),
-             ('parabol',  'hgs_spea2' ),
-             ('parabol',  'hgs_ibea'  ),
-             ('parabol',  'hgs_nsga2' )
+run_rder = [ ('coemoa_d'      , 'hgs_nsga2'     ),  # 896.862s
+             ('kursawe'       , 'hgs_nsga2'     ),  # 867.042s
+             ('coemoa_b'      , 'nsga2'         ),  # 753.007s
+             ('kursawe'       , 'imga_nsga2'    ),  # 731.860s
+             ('coemoa_e'      , 'imga_nsga2'    ),  # 718.351s
+             ('coemoa_e'      , 'hgs_nsga2'     ),  # 710.019s
+             ('kursawe'       , 'nsga2'         ),  # 660.961s
+             ('coemoa_c'      , 'hgs_nsga2'     ),  # 645.418s
+             ('coemoa_c'      , 'imga_nsga2'    ),  # 635.487s
+             ('coemoa_a'      , 'hgs_nsga2'     ),  # 596.658s
+             ('coemoa_b'      , 'hgs_nsga2'     ),  # 572.698s
+             ('coemoa_b'      , 'imga_nsga2'    ),  # 559.691s
+             ('coemoa_a'      , 'imga_nsga2'    ),  # 543.059s
+             ('coemoa_d'      , 'imga_nsga2'    ),  # 416.692s
+             ('coemoa_d'      , 'nsga2'         ),  # 389.425s
+             ('coemoa_e'      , 'nsga2'         ),  # 356.023s
+             ('coemoa_c'      , 'nsga2'         ),  # 322.246s
+             ('coemoa_a'      , 'nsga2'         ),  # 209.462s
+             ('coemoa_d'      , 'ibea'          ),  # 186.222s
+             ('kursawe'       , 'spea2'         ),  # 164.000s
+             ('coemoa_d'      , 'imga_ibea'     ),  # 142.035s
+             ('kursawe'       , 'ibea'          ),  # 111.761s
+             ('coemoa_b'      , 'imga_ibea'     ),  # 107.480s
+             ('kursawe'       , 'imga_ibea'     ),  # 107.008s
+             ('coemoa_a'      , 'ibea'          ),  # 106.333s
+             ('kursawe'       , 'hgs_ibea'      ),  # 106.025s
+             ('coemoa_b'      , 'ibea'          ),  # 105.628s
+             ('coemoa_a'      , 'imga_ibea'     ),  # 102.817s
+             ('kursawe'       , 'imga_spea2'    ),  # 101.375s
+             ('coemoa_b'      , 'hgs_spea2'     ),  # 101.002s
+             ('coemoa_a'      , 'imga_spea2'    ),  #  97.327s
+             ('coemoa_a'      , 'hgs_spea2'     ),  #  96.848s
+             ('coemoa_e'      , 'ibea'          ),  #  95.664s
+             ('coemoa_d'      , 'hgs_ibea'      ),  #  95.239s
+             ('coemoa_d'      , 'imga_spea2'    ),  #  95.203s
+             ('coemoa_e'      , 'imga_ibea'     ),  #  94.125s
+             ('coemoa_b'      , 'imga_spea2'    ),  #  92.748s
+             ('coemoa_d'      , 'hgs_spea2'     ),  #  88.975s
+             ('coemoa_c'      , 'imga_spea2'    ),  #  85.477s
+             ('coemoa_c'      , 'ibea'          ),  #  85.068s
+             ('coemoa_c'      , 'imga_ibea'     ),  #  82.732s
+             ('coemoa_e'      , 'imga_spea2'    ),  #  82.356s
+             ('ackley'        , 'imga_nsga2'    ),  #  80.309s
+             ('coemoa_e'      , 'hgs_ibea'      ),  #  74.197s
+             ('ackley'        , 'hgs_nsga2'     ),  #  73.683s
+             ('coemoa_c'      , 'hgs_ibea'      ),  #  67.489s
+             ('coemoa_b'      , 'hgs_ibea'      ),  #  65.186s
+             ('coemoa_a'      , 'hgs_ibea'      ),  #  65.117s
+             ('coemoa_d'      , 'spea2'         ),  #  60.446s
+             ('kursawe'       , 'hgs_spea2'     ),  #  60.333s
+             ('coemoa_b'      , 'spea2'         ),  #  59.255s
+             ('coemoa_c'      , 'spea2'         ),  #  58.726s
+             ('coemoa_a'      , 'spea2'         ),  #  58.446s
+             ('coemoa_e'      , 'spea2'         ),  #  57.795s
+             ('ackley'        , 'nsga2'         ),  #  49.869s
+             ('coemoa_c'      , 'hgs_spea2'     ),  #  43.404s
+             ('coemoa_e'      , 'hgs_spea2'     ),  #  38.250s
+             ('ackley'        , 'imga_spea2'    ),  #  20.072s
+             ('ackley'        , 'spea2'         ),  #   6.887s
+             ('ackley'        , 'hgs_spea2'     ),  #   5.843s
+             ('parabol'       , 'hgs_ibea'      ),  #   0.011s
+             ('parabol'       , 'hgs_spea2'     ),  #   0.004s
+             ('ackley'        , 'imga_ibea'     ),  #   0.004s
+             ('ackley'        , 'ibea'          ),  #   0.004s
+             ('parabol'       , 'hgs_nsga2'     ),  #   0.003s
+             ('ackley'        , 'hgs_ibea'      )   #   0.003s
            ]
 
