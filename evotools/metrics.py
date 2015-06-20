aac555ca467c533a8d1411de0e03c49a84ede5cc
Konrad Gądek <kgadek@gmail.com>
2015-06-20 23:28:49 +0200

    more logging, some cleanup, prettier output

--- evotools/metrics.py
+++ evotools/metrics.py
@@ -1,32 +1,43 @@
 import itertools
 import math
+from evotools.log_helper import get_logger
+from evotools.timing import log_time, process_time
+
+logger = get_logger(__name__)
 
 
 def distance_from_pareto(solution, pareto):
-    return sum([min([euclid_distance(x, y)
-                     for y in pareto])
-                for x in solution]) / len(solution)
+    solution = list(solution)
+    logger.debug("distance_from_pareto: input length %d", len(solution))
+    with log_time(process_time, logger, "distance_from_pareto computation time: {time_res:.3}s"):
+        return sum([min([euclid_distance(x, y)
+                         for y in pareto])
+                    for x in solution]) / len(solution)
 
 
 def distribution(solution, sigma=0.5):
-    solutionL = list(solution)
-    return sum(sum(1
-                   for y in solutionL
-                   if euclid_distance(x, y) > sigma
-                   ) / (len(solutionL) - 1)
-               for x in solutionL
-               ) / len(solutionL)
+    solution = list(solution)
+    logger.debug("distribution: input length %d", len(solution))
+    with log_time(process_time, logger, "distribution computation time: {time_res:.3}s"):
+        return sum(sum(1
+                       for y in solution
+                       if sigma < math.sqrt(sum((x1 - y1) ** 2 for x1, y1 in zip(x, y)))
+                       )
+                   for x in solution
+                   ) / (len(solution) * (len(solution) - 1))
 
 
 def extent(solution):
-    return math.sqrt(sum(max(math.fabs(x[i] - y[i])
-                             for x, y
-                             in itertools.product(solution, solution)
-                            )
-                         for i
-                         in range(len(solution[0]))
+    logger.debug("extent: input length %d", len(solution))
+    with log_time(process_time, logger, "extent computation time: {time_res:.3}s"):
+        return math.sqrt(sum(max(math.fabs(x[i] - y[i])
+                                 for x in solution
+                                 for y in solution
+                                 )
+                             for i
+                             in range(len(solution[0]))
+                             )
                          )
-                     )
 
 
 def euclid_distance(xs, ys):
